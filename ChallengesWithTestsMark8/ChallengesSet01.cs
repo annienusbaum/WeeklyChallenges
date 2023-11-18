d62efd3a93ff127f8395dc86441883e91bad8290
Annie Nusbaum <annie.nusbaum.83@gmail.com>
2023-11-18 09:50:14 -0500

    WeeklyChallengeOne

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -15,35 +15,58 @@ namespace ChallengesWithTestsMark8
                 return false;
             }
         }
+        //done!
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            return minuend - subtrahend;
         }
+
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            return number1 + number2;
         }
+
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            if(number1 < number2)
+            {
+                return number1;
+            }
+            else
+            {
+                return number2;
+            }
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            return factor1 * factor2;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            if (nameOfPerson == "")
+            {
+                return "Hello!";
+            }
+            else
+            {
+                return $"Hello, {nameOfPerson}!";
+            }
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            return "HEY!";
         }
     }
 }
+
+
+//look at method name "GetGreeting" and ask what does it want from me?
+//What's it giving me? by looking at the parameter - so (string nameOfPerson)
+//and ask what does it want from me? by looking at the return type (string)
+//
